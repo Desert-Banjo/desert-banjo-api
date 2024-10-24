a13b942f04552d70b7278b5c30e60934e22bfced
Fardowsa I Abdalla <fardowsaabdalla14@gmail.com>
2024-10-24 19:53:34 -0400

    Steps 3 and 4 completed for Part 1

--- Desert-Banjo.Api/CatalogController.cs
+++ Desert-Banjo.Api/CatalogController.cs
@@ -10,13 +10,28 @@ namespace Desert.Banjo.Api.Controllers
         [HttpGet]
         public IActionResult GetItems()
         {
-        var items = new List<Item>()
-        {
-            new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
-            new Item("Shorts", "Ohio State shorts.", "NIke", 44.99m)
-        
-        };
-    return Ok(items);
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "NIke", 44.99m)
+            };
+            return Ok(items);
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+
+            return Ok(item);
+        }
+        
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
+
     }
- }
+}
--- Desert-Banjo.Domain/Catalog/item.cs
+++ Desert-Banjo.Domain/Catalog/item.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Desert.Banjo.Domain.Catalog
 {
-    public class Item 
+    public class Item
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -10,38 +10,38 @@ namespace Desert.Banjo.Domain.Catalog
         public string Brand { get; set; }
         public decimal Price { get; set; }
         public List<Rating> Ratings { get; set; } = new List<Rating>();
-         public void AddRating(Rating rating)
-    {
-        this.Ratings.Add(rating);
-    }
-
- 
-public Item(string name, string description, string brand, decimal price)
-    {
-        if(string.IsNullOrEmpty(name))
+        public void AddRating(Rating rating)
         {
-            throw new ArgumentNullException(name);
-        }
-        
-        if (string.IsNullOrEmpty(description))
-        {
-            throw new ArgumentException(description);
-        }
-        
-        if (string.IsNullOrEmpty(brand))
-        {
-            throw new ArgumentException(brand);
+            this.Ratings.Add(rating);
         }
 
-        if(price < 0.00m) 
+
+        public Item(string name, string description, string brand, decimal price)
         {
-            throw new ArgumentException("Price must be greater than zero.");
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(name);
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentException(description);
+            }
+
+            if (string.IsNullOrEmpty(brand))
+            {
+                throw new ArgumentException(brand);
+            }
+
+            if (price < 0.00m)
+            {
+                throw new ArgumentException("Price must be greater than zero.");
+            }
+            Name = name;
+            Description = description;
+            Brand = brand;
+            Price = price;
         }
-        Name = name;
-        Description = description;
-        Brand = brand;
-        Price = price;
-    }
 
 
 
