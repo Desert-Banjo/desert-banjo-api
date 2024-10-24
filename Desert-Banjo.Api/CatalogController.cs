ac00db867df3cfcaef299490589ea66c6c74d6aa
haileigh112 <stanton.241@buckeyemail.osu.edu>
2024-10-24 18:37:25 -0400

    step 2 done

--- Desert-Banjo.Api/CatalogController.cs
+++ Desert-Banjo.Api/CatalogController.cs
@@ -10,7 +10,13 @@ namespace Desert.Banjo.Api.Controllers
         [HttpGet]
         public IActionResult GetItems()
         {
-            return Ok("hello world.");
+        var items = new List<Item>()
+        {
+            new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+            new Item("Shorts", "Ohio State shorts.", "NIke", 44.99m)
+        
+        };
+    return Ok(items);
         }
     }
-}
+ }
