28c401370fa42a5ef7a255056acaa9a9413f28fc
haileigh112 <stanton.241@buckeyemail.osu.edu>
2024-11-14 18:56:48 -0500

    Fixed items table not found error

--- Desert-Banjo.Api/CatalogController.cs
+++ Desert-Banjo.Api/CatalogController.cs
@@ -10,73 +10,87 @@ namespace Desert.Banjo.Api.Controllers
     [Route("[controller]")]
     public class CatalogController : ControllerBase
     {
-        private readonly StoreContext _db;
-        public CatalogController(StoreContext db)
-        {
-            _db = db;
-        }
+        // private readonly StoreContext _db;
+        // public CatalogController(StoreContext db)
+        // {
+        //     _db = db;
+        // }
 
         [HttpGet]
         public IActionResult GetItems()
         {
-            return Ok(_db.Items);
+            //return Ok("Hello World--");
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State Shirt", "Nike", 39.99m),
+                new Item("Shorts", "Ohio State shorts", "Nike", 49.99m)
+            };
+            return Ok(items);
+            //return Ok(_db.Items);
         }
 
         [HttpGet("{id:int}")]
         public IActionResult GetItem(int id)
         {
-           var item = _db.Items.Find(id);
-           if (item == null)
-           {
-            return NotFound();
-           }
-           return Ok();
+           var item = new Item("Shirt", "Ohio State Shirt", "Nike", 59.99m);
+           item.Id = id;
+           
+           // var item = _db.Items.Find(id);
+           // if (item == null)
+           // {
+           //   return NotFound();
+           // }
+           return Ok(item);
        
         }
 
         [HttpPost]
-        public IActionResult Post(Item item)
+        public IActionResult CreateItem (Item item)
         {
-            _db.Items.Add(item);
-            _db.SaveChanges();
-            return Created($"/catalog/{item.Id}",item);
+            // return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
+            return Created("/catalog/42", item);
+            // _db.Item.Add(Item);
+            // _db.SaveChange();
+            // return Created($"/catalog/{item.Id}" item);
         }
 
         [HttpPost("{id:int}/ratings")]
         public IActionResult PostRating(int id, [FromBody] Rating rating)
         {
-           var item = _db.Items.Find(id);
-           if (item == null){
-            return NotFound();
-           }
-           item.AddRating(rating);
-           _db.SaveChanges();
-            return Ok(item);
+        //    var item = _db.Items.Find(id);
+        //    if (item == null){
+        //     return NotFound();
+        //    }
+        //    item.AddRating(rating);
+        //    _db.SaveChanges();
+        //     return Ok(item);
+        return Ok();
         }
+
         [HttpPut("{id:int}")]
         public IActionResult PutItem(int id,[FromBody] Item item)
         {
-            if( id!= item.Id){
-                return BadRequest();
-            }
-            if(_db.Items.Find(id)== null)
-            {
-                return NotFound();
-            }
-            _db.Entry(item).State = EntityState.Modified; 
-            _db.SaveChanges();
+            // if( id!= item.Id){
+            //     return BadRequest();
+            // }
+            // if(_db.Items.Find(id)== null)
+            // {
+            //     return NotFound();
+            // }
+            // _db.Entry(item).State = EntityState.Modified; 
+            // _db.SaveChanges();
             return NoContent();
         }
 
         [HttpDelete("{id:int}")]
-        public IActionResult DeleteItem(int id)
+        public IActionResult Delete(int id)
         {
-            var item =_db.Items.Find(id);
-            if(item ==null){
-            return NotFound();
-            }
-            _db.Items.Remove(item);
-            _db.SaveChanges();
+            // var item =_db.Items.Find(id);
+            // if(item ==null){
+            // return NotFound();
+            // }
+            // _db.Items.Remove(item);
+            // _db.SaveChanges();
             return Ok();
         }
     }
